51eebc01e611b111d5f437fd3c38fcf96d0e1089
meteomod <115956148+TMarilynMamani@users.noreply.github.com>
2023-01-11 13:10:23 -0400

    Create ALPACOMA.R

---
+++ ALPACOMA.R
@@ -0,0 +1,31 @@
+# ---------------------------------------------------------------------
+# -------------------- MONITOREO OLORES - ALPACOMA --------------------
+# ---------------------------------------------------------------------
+# --------- cargado de librerias -----------
+library(readxl)
+library(dplyr)
+library(ggplot2) #graficos
+library(wesanderson) #paletas de colores
+library(lubridate) #fechas
+library(tidyverse)
+library(stringr)
+library(lattice)
+library(plyr)
+library(gridExtra)
+library(openair)
+library(devtools)
+# ------------------------------------------------------------------------------
+# -------- 1) IMPORTAR DESDE EXCEL Y PASAR LOS DATOS A DATAFRAME ---------------
+# ------------------------------------------------------------------------------
+ALP_2023 <- read.csv(file = "ALPACOMA_METEREOLOGIA.csv", header = TRUE, sep = ";", dec = ",") 
+
+ALP_2023$date <- as.POSIXct(ALP_2023$date,format="%d/%m/%Y %H:%M",tz="America/La_Paz")
+ALP_2023$ws <- as.numeric(ALP_2023$ws) 
+ALP_2023$wd <- as.numeric(ALP_2023$wd)
+str(ALP_2023)
+# ------------------------------------------------------------------------------
+# ------------------ 6) GRAFICOS ELABORADOS CON OPENAIR  -----------------------
+# ------------------------------------------------------------------------------
+trendLevel(ALP_2023, pollutant = "ws") #velocidad del viento
+trendLevel(ALP_2023, pollutant = "wd") #direccion del viento
+
