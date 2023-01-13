308d4de8378172d0b7f49038bc7ada2031cc331b
TMarilynMamani <iqaamary@gmail.com>
2023-01-13 12:15:17 -0400

    CAMBIOS 13/01/2023

--- ALPACOMA.R
+++ ALPACOMA.R
@@ -23,9 +23,27 @@ ALP_2023$date <- as.POSIXct(ALP_2023$date,format="%d/%m/%Y %H:%M",tz="America/La
 ALP_2023$ws <- as.numeric(ALP_2023$ws) 
 ALP_2023$wd <- as.numeric(ALP_2023$wd)
 str(ALP_2023)
+
+
+ALP_2023_VF <- read.csv(file = "LPZ_ALPACOMA_2023.csv", header = TRUE, sep = ";", dec = ",") 
+
+ALP_2023_VF$date <- as.POSIXct(ALP_2023_VF$date,format="%d/%m/%Y %H:%M",tz="America/La_Paz")
+ALP_2023_VF$Rainfall <- as.numeric(ALP_2023_VF$Rainfall) 
+ALP_2023_VF$WindSpd.Avg <- as.numeric(ALP_2023_VF$WindSpd.Avg)
+ALP_2023_VF$WindDir.Avg <- as.numeric(ALP_2023_VF$WindDir.Avg)
+ALP_2023_VF$Radiation.Avg <- as.numeric(ALP_2023_VF$Radiation.Avg)
+ALP_2023_VF$AirTemp.Avg <- as.numeric(ALP_2023_VF$AirTemp.Avg)
+ALP_2023_VF$Humidity.Avg <- as.numeric(ALP_2023_VF$Humidity.Avg)
+str(ALP_2023_VF)
 # ------------------------------------------------------------------------------
 # ------------------ 2) GRAFICOS ELABORADOS CON OPENAIR  -----------------------
 # ------------------------------------------------------------------------------
 trendLevel(ALP_2023, pollutant = "ws") #velocidad del viento
 trendLevel(ALP_2023, pollutant = "wd") #direccion del viento
 
+trendLevel(ALP_2023_VF, pollutant = "Rainfall") #precipitacion [mm]
+trendLevel(ALP_2023_VF, pollutant = "WindSpd.Avg") #velocidad del viento [m/s]
+trendLevel(ALP_2023_VF, pollutant = "WindDir.Avg") #direccion del viento [°]
+trendLevel(ALP_2023_VF, pollutant = "Radiation.Avg") #radiacion [W/m2]
+trendLevel(ALP_2023_VF, pollutant = "AirTemp.Avg") #temperatura del aire [°C]
+trendLevel(ALP_2023_VF, pollutant = "Humidity.Avg") #Humedad [%]
