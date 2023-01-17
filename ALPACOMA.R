c6dd34f733ef6b77fc07166b9edc7325138c4e5f
TMarilynMamani <iqaamary@gmail.com>
2023-01-17 00:00:19 -0400

    CAMBIOS 16/01/2023

--- ALPACOMA.R
+++ ALPACOMA.R
@@ -1,6 +1,6 @@
-# ---------------------------------------------------------------------
-# -------------------- MONITOREO OLORES - ALPACOMA --------------------
-# ---------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------- MONITOREO OLORES - ALPACOMA ------------------------
+# ------------------------------------------------------------------------------
 # --------- cargado de librerias -----------
 library(readxl)
 library(dplyr)
@@ -15,7 +15,7 @@ library(gridExtra)
 library(openair)
 library(devtools)
 # ------------------------------------------------------------------------------
-# -------- 1) IMPORTAR DESDE EXCEL Y PASAR LOS DATOS A DATAFRAME ---------------
+# ----------------------- METEREOLOGIA ALPACOMA  --------------------------
 # ------------------------------------------------------------------------------
 ALP_2023 <- read.csv(file = "ALPACOMA_METEREOLOGIA.csv", header = TRUE, sep = ";", dec = ",") 
 
@@ -24,26 +24,47 @@ ALP_2023$ws <- as.numeric(ALP_2023$ws)
 ALP_2023$wd <- as.numeric(ALP_2023$wd)
 str(ALP_2023)
 
-
-ALP_2023_VF <- read.csv(file = "LPZ_ALPACOMA_2023.csv", header = TRUE, sep = ";", dec = ",") 
-
-ALP_2023_VF$date <- as.POSIXct(ALP_2023_VF$date,format="%d/%m/%Y %H:%M",tz="America/La_Paz")
-ALP_2023_VF$Rainfall <- as.numeric(ALP_2023_VF$Rainfall) 
-ALP_2023_VF$WindSpd.Avg <- as.numeric(ALP_2023_VF$WindSpd.Avg)
-ALP_2023_VF$WindDir.Avg <- as.numeric(ALP_2023_VF$WindDir.Avg)
-ALP_2023_VF$Radiation.Avg <- as.numeric(ALP_2023_VF$Radiation.Avg)
-ALP_2023_VF$AirTemp.Avg <- as.numeric(ALP_2023_VF$AirTemp.Avg)
-ALP_2023_VF$Humidity.Avg <- as.numeric(ALP_2023_VF$Humidity.Avg)
-str(ALP_2023_VF)
-# ------------------------------------------------------------------------------
-# ------------------ 2) GRAFICOS ELABORADOS CON OPENAIR  -----------------------
-# ------------------------------------------------------------------------------
 trendLevel(ALP_2023, pollutant = "ws") #velocidad del viento
 trendLevel(ALP_2023, pollutant = "wd") #direccion del viento
 
-trendLevel(ALP_2023_VF, pollutant = "Rainfall") #precipitacion [mm]
-trendLevel(ALP_2023_VF, pollutant = "WindSpd.Avg") #velocidad del viento [m/s]
-trendLevel(ALP_2023_VF, pollutant = "WindDir.Avg") #direccion del viento [°]
-trendLevel(ALP_2023_VF, pollutant = "Radiation.Avg") #radiacion [W/m2]
-trendLevel(ALP_2023_VF, pollutant = "AirTemp.Avg") #temperatura del aire [°C]
-trendLevel(ALP_2023_VF, pollutant = "Humidity.Avg") #Humedad [%]
+# ------------------------------------------------------------------------------
+# ----------------------- METEREOLOGIA ALPACOMA 2021  --------------------------
+# ------------------------------------------------------------------------------
+ALP_2021_VF <- read.csv(file = "LPZ_ALPACOMA_2021.csv", header = TRUE, sep = ";", dec = ",") 
+
+ALP_2021_VF$date <- as.POSIXct(ALP_2021_VF$date,format="%d/%m/%Y %H:%M",tz="America/La_Paz")
+ALP_2021_VF$Rainfall <- as.numeric(ALP_2021_VF$Rainfall) 
+ALP_2021_VF$WindSpd.Avg <- as.numeric(ALP_2021_VF$WindSpd.Avg)
+ALP_2021_VF$WindDir.Avg <- as.numeric(ALP_2021_VF$WindDir.Avg)
+ALP_2021_VF$Radiation.Avg <- as.numeric(ALP_2021_VF$Radiation.Avg)
+ALP_2021_VF$AirTemp.Avg <- as.numeric(ALP_2021_VF$AirTemp.Avg)
+ALP_2021_VF$Humidity.Avg <- as.numeric(ALP_2021_VF$Humidity.Avg)
+str(ALP_2021_VF)
+
+trendLevel(ALP_2021_VF, pollutant = "Rainfall") #precipitacion [mm]
+trendLevel(ALP_2021_VF, pollutant = "WindSpd.Avg") #velocidad del viento [m/s]
+trendLevel(ALP_2021_VF, pollutant = "WindDir.Avg") #direccion del viento [°]
+trendLevel(ALP_2021_VF, pollutant = "Radiation.Avg") #radiacion [W/m2]
+trendLevel(ALP_2021_VF, pollutant = "AirTemp.Avg") #temperatura del aire [°C]
+trendLevel(ALP_2021_VF, pollutant = "Humidity.Avg") #Humedad [%]
+
+# ------------------------------------------------------------------------------
+# ----------------------- METEREOLOGIA ALPACOMA 2022  --------------------------
+# ------------------------------------------------------------------------------
+ALP_2022_VF <- read.csv(file = "LPZ_ALPACOMA_2022.csv", header = TRUE, sep = ";", dec = ",") 
+
+ALP_2022_VF$date <- as.POSIXct(ALP_2022_VF$date,format="%d/%m/%Y %H:%M",tz="America/La_Paz")
+ALP_2022_VF$Rainfall <- as.numeric(ALP_2022_VF$Rainfall) 
+ALP_2022_VF$WindSpd.Avg <- as.numeric(ALP_2022_VF$WindSpd.Avg)
+ALP_2022_VF$WindDir.Avg <- as.numeric(ALP_2022_VF$WindDir.Avg)
+ALP_2022_VF$Radiation.Avg <- as.numeric(ALP_2022_VF$Radiation.Avg)
+ALP_2022_VF$AirTemp.Avg <- as.numeric(ALP_2022_VF$AirTemp.Avg)
+ALP_2022_VF$Humidity.Avg <- as.numeric(ALP_2022_VF$Humidity.Avg)
+str(ALP_2022_VF)
+
+trendLevel(ALP_2022_VF, pollutant = "Rainfall") #precipitacion [mm]
+trendLevel(ALP_2022_VF, pollutant = "WindSpd.Avg") #velocidad del viento [m/s]
+trendLevel(ALP_2022_VF, pollutant = "WindDir.Avg") #direccion del viento [°]
+trendLevel(ALP_2022_VF, pollutant = "Radiation.Avg") #radiacion [W/m2]
+trendLevel(ALP_2022_VF, pollutant = "AirTemp.Avg") #temperatura del aire [°C]
+trendLevel(ALP_2022_VF, pollutant = "Humidity.Avg") #Humedad [%]
